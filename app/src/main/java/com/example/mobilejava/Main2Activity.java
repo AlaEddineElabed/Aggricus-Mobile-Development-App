4758ea8204adaef7f17c89d04098e0adaf18aa78
ska3rfon <azizchabchoub@yahoo.fr>
2021-08-13 00:42:02 +0100

    ska3

---
+++ app/src/main/java/com/example/mobilejava/Main2Activity.java
@@ -0,0 +1,34 @@
+package com.example.mobilejava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        final DrawerLayout drawerLayout=findViewById(R.id.drawer_layout) ;
+        findViewById(R.id.imageview5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+}}
