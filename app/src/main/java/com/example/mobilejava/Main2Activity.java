3adfa42826e4642929b2d1f58595817d30c2c6a4
ska3rfon <azizchabchoub@yahoo.fr>
2021-08-16 02:55:08 +0100

    slider and cardviews

--- app/src/main/java/com/example/mobilejava/Main2Activity.java
+++ app/src/main/java/com/example/mobilejava/Main2Activity.java
@@ -1,34 +1,93 @@
 package com.example.mobilejava;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.provider.ContactsContract;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity  {
 
+
+private ViewPager viewPager;
+private ArrayList<MyModel> modelArrayList;
+private CardViewAdapter adapter ;
+SliderView sliderView;
+int[]images={R.drawable.image1,R.drawable.image2,R.drawable.image3,R.drawable.image4};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        final DrawerLayout drawerLayout=findViewById(R.id.drawer_layout) ;
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout) ;
+        sliderView=findViewById(R.id.image_slider);
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
+
+        viewPager=findViewById(R.id.viewerpage);
+        loadcard();
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
         findViewById(R.id.imageview5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
+
         });
+        
 
+}
 
-}}
+    private void loadcard() {
+        modelArrayList =new ArrayList<>();
+        modelArrayList.add(new MyModel("xxx","fsdf",R.drawable.image1));
+        modelArrayList.add(new MyModel("xxx","esgsfdsfs",R.drawable.image2));
+        modelArrayList.add(new MyModel("xsgx","dsfs",R.drawable.image3));
+        adapter=new CardViewAdapter(this,modelArrayList);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(100,0,100,0);
+    }
+}
